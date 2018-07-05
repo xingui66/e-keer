d698bf7d9d2f14723d1b5a34667062c7c069637f
xingui66 <xingui66@126.com>
2018-07-05 16:32:32 +0800

    1.添加后台管理页面 2.查询商品列表，并显示数据(上一页下一页总数等数据)

--- e-manager/e-manager-interface/src/main/java/com/keer/service/ItemService.java
+++ e-manager/e-manager-interface/src/main/java/com/keer/service/ItemService.java
@@ -1,8 +1,10 @@
 package com.keer.service;
 
+import com.keer.common.pojo.EasyUIDataGridResult;
 import com.keer.pojo.TbItem;
 
 public interface ItemService {
 	TbItem getItemById(long item);
+	EasyUIDataGridResult getItemList(int page,int rows);
 
 }
--- e-manager/e-manager-service/src/main/java/com/keer/service/impl/ItemServiceImpl.java
+++ e-manager/e-manager-service/src/main/java/com/keer/service/impl/ItemServiceImpl.java
@@ -3,8 +3,13 @@ package com.keer.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.keer.common.pojo.EasyUIDataGridResult;
 import com.keer.mapper.TbItemMapper;
 import com.keer.pojo.TbItem;
 import com.keer.pojo.TbItemExample;
@@ -27,6 +32,29 @@ public class ItemServiceImpl implements ItemService {
 			return list.get(0);
 		}
 		return null;
+	}
+
+	@Override
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+		/*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		//获得Mapper的代理对象
+		TbItemMapper itemMapper = applicationContext.getBean(TbItemMapper.class);*/
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//取分页信息
+		//PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setRows(list);
+		
+		//取分页结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		//取总记录数
+		long total = pageInfo.getTotal();
+		result.setTotal(total);
+		return result;
 	}
 
 }
